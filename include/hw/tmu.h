b640d756f2207e65beb1e1d8b94b0f388b23e1b8
lekernel <sebastien.bourdeauducq@lekernel.net>
2009-10-17 16:25:09 +0200

    Skeleton

---
+++ include/hw/tmu.h
@@ -0,0 +1,31 @@
+/*
+ * Milkymist Emulation Libraries
+ * Copyright (C) 2007, 2008, 2009 Sebastien Bourdeauducq
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __HW_TMU_H
+#define __HW_TMU_H
+
+#define TMU_BRIGHTNESS_MAX	(63)
+
+struct tmu_vertex {
+	short int y;
+	short int x;
+} __attribute__((packed));
+
+#define TMU_MESH_MAXSIZE	128
+
+#endif /* __HW_TMU_H */
+
